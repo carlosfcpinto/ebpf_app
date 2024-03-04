80000635564cb648bba1a96f299ef5d71da4a469
carlosfcpinto <carlosfranciscopintoscp@gmail.com>
2024-03-04 18:53:48 +0000

    bats-core testing

--- src/test.sh
+++ src/test.sh
@@ -5,7 +5,7 @@
     touch testfile
 
     # Set the file ownership to user with ID 1006
-    chown 1006 testfile
+    chown 1001 testfile
 
     # Attempt to change the file mode as the user with ID 1006
     run su -c "chmod 777 testfile" -s /bin/bash test2
@@ -18,7 +18,7 @@
     touch testfile2
 
     #testfile2 is whitelisted for user 1006 in ebpf program, so the chmod system call should be allowed
-    chown 1006 testfile2
+    chown 1001 testfile2
     run su -c "chmod 777 testfile2" -s /bin/bash test2
 
     rm testfile2
