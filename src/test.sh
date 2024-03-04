e83654876a5e4a88530db922bcf83a79852fcb0c
carlosfcpinto <carlosfranciscopintoscp@gmail.com>
2024-03-04 18:57:45 +0000

    bats testing specific for github

--- src/test.sh
+++ src/test.sh
@@ -8,7 +8,7 @@
     chown 1001 testfile
 
     # Attempt to change the file mode as the user with ID 1006
-    run su -c "chmod 777 testfile" -s /bin/bash test2
+    run su -c "chmod 777 testfile" -s /bin/bash $(id -nu 1001)
 
     rm testfile
     [ "$status" -ne 0 ]
@@ -19,7 +19,7 @@
 
     #testfile2 is whitelisted for user 1006 in ebpf program, so the chmod system call should be allowed
     chown 1001 testfile2
-    run su -c "chmod 777 testfile2" -s /bin/bash test2
+    run su -c "chmod 777 testfile2" -s /bin/bash $(id -nu 1001)
 
     rm testfile2
     [ "$status" -eq 0 ]
