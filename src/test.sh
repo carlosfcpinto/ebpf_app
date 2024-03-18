773de0d5550573a6e6694393aca3760da1409080
carlosfcpinto <carlosfranciscopintoscp@gmail.com>
2024-03-18 18:33:08 +0000

    bats not working

--- src/test.sh
+++ src/test.sh
@@ -5,11 +5,11 @@
     touch testfile
 
     # Set the file ownership to user with ID 1006
-    # chown 1001 testfile
+    chown 1001 testfile
 
     # Attempt to change the file mode as the user with ID 1006
-    # run su -c "chmod 777 testfile" -s /bin/bash $(id -nu 1001)
-    run chmod 777 testfile
+    run su -c "chmod 777 testfile" -s /bin/bash $(id -nu 1001)
+    # run chmod 777 testfile
 
     rm testfile
     [ "$status" -ne 0 ]
@@ -19,9 +19,9 @@
     touch testfile2
 
     #testfile2 is whitelisted for user 1006 in ebpf program, so the chmod system call should be allowed
-    # chown 1001 testfile2
-    # run su -c "chmod 777 testfile2" -s /bin/bash $(id -nu 1001)
-    run chmod 777 testfile2
+    chown 1001 testfile2
+    run su -c "chmod 777 testfile2" -s /bin/bash $(id -nu 1001)
+    # run chmod 777 testfile2
 
     rm testfile2
     [ "$status" -eq 0 ]
